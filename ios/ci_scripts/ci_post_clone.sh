f63f2f60286ab1e7c8efcd373afb4491d1994102
jinlee1978 <30416679+jinlee1978@users.noreply.github.com>
2026-02-17 19:21:35 -0800

    Fix: Move agvtool to correct ios directory

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
-# 1. Move to project root
-cd ../..
+# 1. Move to the 'ios' folder where your .xcodeproj lives
+cd ..
 
-# 2. Automate the build number using the Xcode Cloud Environment Variable
-# This ensures the version in the cloud matches the Build Number Apple expects
+# 2. Update the build number (agvtool will now find the project)
 agvtool new-version -all $CI_BUILD_NUMBER
 
-# 3. Standard setup
+# 3. Now move to the project root for npm/pods
+cd ..
 brew install node cocoapods
 npm install
 cd ios && pod install
